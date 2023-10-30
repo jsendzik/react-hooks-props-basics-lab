f5b4d76210b6c374873da5a458569ae9a51b3d62
Jacob Sendzik <jake.sendzik@gmail.com>
2023-10-30 15:16:24 -0400

    complete now

--- src/components/Links.js
+++ src/components/Links.js
@@ -4,7 +4,7 @@ function Links(props) {
     return (
         <div id="links">
             <h3>Links</h3>
-            <a href={props.gihub}>{props.github}</a>
+            <a href={props.github}>{props.github}</a>
             <a href={props.linkedin}>{props.linkedin}</a>
         </div>
     )
